51e247a5224e426a22a4c3b347255279f8845327
Pablo Castellano <pablo@anche.no>
2025-06-26 20:38:00 +0200

    fix: Batch is never full when is mutable (#4)
    * fix: Batch is never full when is mutable
    
    * fix: signature

--- src/stamper.ts
+++ src/stamper.ts
@@ -8,23 +8,25 @@ export class Stamper {
     address: Uint8Array
     depth: number
     maxSlot: number
+    immutable: boolean
 
-    private constructor(signer: bigint, batchId: Uint8Array, buckets: Uint32Array, depth: number) {
+    private constructor(signer: bigint, batchId: Uint8Array, buckets: Uint32Array, depth: number, immutable: boolean) {
         this.signer = new PrivateKey(Binary.numberToUint256(signer, 'BE'))
         this.batchId = batchId
         this.buckets = buckets
         this.depth = depth
+        this.immutable = immutable
         this.maxSlot = 2 ** (this.depth - 16)
         const publicKey = Elliptic.privateKeyToPublicKey(signer)
         this.address = Elliptic.publicKeyToAddress(publicKey)
     }
 
-    static fromBlank(signer: bigint, batchId: Uint8Array, depth: number) {
-        return new Stamper(signer, batchId, new Uint32Array(65536), depth)
+    static fromBlank(signer: bigint, batchId: Uint8Array, depth: number, immutable: boolean) {
+        return new Stamper(signer, batchId, new Uint32Array(65536), depth, immutable)
     }
 
-    static fromState(signer: bigint, batchId: Uint8Array, buckets: Uint32Array, depth: number) {
-        return new Stamper(signer, batchId, buckets, depth)
+    static fromState(signer: bigint, batchId: Uint8Array, buckets: Uint32Array, depth: number, immutable: boolean) {
+        return new Stamper(signer, batchId, buckets, depth, immutable)
     }
 
     stamp(chunk: Chunk) {
@@ -32,8 +34,8 @@ export class Stamper {
         const bucket = Binary.uint16ToNumber(address, 'BE')
         const height = this.buckets[bucket]
 
-        if (height >= this.maxSlot) {
-            throw Error(`Batch is full: ${this.batchId}`)
+        if (height >= this.maxSlot && this.immutable) {
+            throw Error(`Batch is full: ${new BatchId(this.batchId)}`)
         }
 
         this.buckets[bucket]++
@@ -41,7 +43,7 @@ export class Stamper {
         const timestamp = Binary.numberToUint64(BigInt(Date.now()), 'BE')
         const message = Binary.concatBytes(address, this.batchId, index, timestamp)
         const signature = this.signer.sign(message)
-        
+
         return {
             batchId: new BatchId(this.batchId),
             index,
